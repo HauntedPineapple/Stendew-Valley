3d3d2cae9cf7448aacdb9b2fe9b16d2e66695162
jpdoyle96 <jpdoyle96@gmail.com>
2023-10-04 18:52:06 -0400

    Made content patcher folder and added the MainIsland map

---
+++ StendewValley/[CP] StendewValley_ContentPatcher/MapAssets/IslandTilesheet1.tsx
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="IslandTilesheet1" tilewidth="16" tileheight="16" tilecount="2080" columns="32">
+ <image source="./island_tilesheet_1.png" width="512" height="1040"/>
+ <tile id="261">
+  <animation>
+   <frame tileid="261" duration="300"/>
+   <frame tileid="262" duration="300"/>
+   <frame tileid="263" duration="300"/>
+   <frame tileid="264" duration="300"/>
+   <frame tileid="265" duration="300"/>
+   <frame tileid="266" duration="300"/>
+   <frame tileid="267" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="293">
+  <animation>
+   <frame tileid="293" duration="300"/>
+   <frame tileid="294" duration="300"/>
+   <frame tileid="295" duration="300"/>
+   <frame tileid="296" duration="300"/>
+   <frame tileid="297" duration="300"/>
+   <frame tileid="298" duration="300"/>
+   <frame tileid="299" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="325">
+  <animation>
+   <frame tileid="325" duration="300"/>
+   <frame tileid="326" duration="300"/>
+   <frame tileid="327" duration="300"/>
+   <frame tileid="328" duration="300"/>
+   <frame tileid="329" duration="300"/>
+   <frame tileid="330" duration="300"/>
+   <frame tileid="331" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="357">
+  <animation>
+   <frame tileid="357" duration="300"/>
+   <frame tileid="358" duration="300"/>
+   <frame tileid="359" duration="300"/>
+   <frame tileid="360" duration="300"/>
+   <frame tileid="361" duration="300"/>
+   <frame tileid="362" duration="300"/>
+   <frame tileid="363" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="389">
+  <animation>
+   <frame tileid="389" duration="300"/>
+   <frame tileid="390" duration="300"/>
+   <frame tileid="391" duration="300"/>
+   <frame tileid="392" duration="300"/>
+   <frame tileid="393" duration="300"/>
+   <frame tileid="394" duration="300"/>
+   <frame tileid="395" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="421">
+  <animation>
+   <frame tileid="421" duration="300"/>
+   <frame tileid="422" duration="300"/>
+   <frame tileid="423" duration="300"/>
+   <frame tileid="424" duration="300"/>
+   <frame tileid="425" duration="300"/>
+   <frame tileid="426" duration="300"/>
+   <frame tileid="427" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="453">
+  <animation>
+   <frame tileid="453" duration="300"/>
+   <frame tileid="454" duration="300"/>
+   <frame tileid="455" duration="300"/>
+   <frame tileid="456" duration="300"/>
+   <frame tileid="457" duration="300"/>
+   <frame tileid="458" duration="300"/>
+   <frame tileid="459" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="485">
+  <animation>
+   <frame tileid="485" duration="300"/>
+   <frame tileid="486" duration="300"/>
+   <frame tileid="487" duration="300"/>
+   <frame tileid="488" duration="300"/>
+   <frame tileid="489" duration="300"/>
+   <frame tileid="490" duration="300"/>
+   <frame tileid="491" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="651">
+  <animation>
+   <frame tileid="651" duration="300"/>
+   <frame tileid="652" duration="300"/>
+   <frame tileid="653" duration="300"/>
+   <frame tileid="654" duration="300"/>
+   <frame tileid="655" duration="300"/>
+   <frame tileid="656" duration="300"/>
+   <frame tileid="657" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="683">
+  <animation>
+   <frame tileid="683" duration="300"/>
+   <frame tileid="684" duration="300"/>
+   <frame tileid="685" duration="300"/>
+   <frame tileid="686" duration="300"/>
+   <frame tileid="687" duration="300"/>
+   <frame tileid="688" duration="300"/>
+   <frame tileid="689" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="715">
+  <animation>
+   <frame tileid="715" duration="300"/>
+   <frame tileid="716" duration="300"/>
+   <frame tileid="717" duration="300"/>
+   <frame tileid="718" duration="300"/>
+   <frame tileid="719" duration="300"/>
+   <frame tileid="720" duration="300"/>
+   <frame tileid="721" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="747">
+  <animation>
+   <frame tileid="747" duration="300"/>
+   <frame tileid="748" duration="300"/>
+   <frame tileid="749" duration="300"/>
+   <frame tileid="750" duration="300"/>
+   <frame tileid="751" duration="300"/>
+   <frame tileid="752" duration="300"/>
+   <frame tileid="753" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1238">
+  <animation>
+   <frame tileid="1238" duration="300"/>
+   <frame tileid="1239" duration="300"/>
+   <frame tileid="1240" duration="300"/>
+   <frame tileid="1241" duration="300"/>
+   <frame tileid="1242" duration="300"/>
+   <frame tileid="1243" duration="300"/>
+   <frame tileid="1244" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1270">
+  <animation>
+   <frame tileid="1270" duration="300"/>
+   <frame tileid="1271" duration="300"/>
+   <frame tileid="1272" duration="300"/>
+   <frame tileid="1273" duration="300"/>
+   <frame tileid="1274" duration="300"/>
+   <frame tileid="1275" duration="300"/>
+   <frame tileid="1276" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
